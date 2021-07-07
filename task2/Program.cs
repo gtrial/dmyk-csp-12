298250823f88ed516ed5c147876d8652dea364f6
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-07-07 10:00:20 +0300

    Initial commit

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task3
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
